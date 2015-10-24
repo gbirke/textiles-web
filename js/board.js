6b0782a49e5ed0df9ed1f7076dc20d4f3680fc84
Gabriel Birke <gb@birke-software.de>
2015-10-24 17:50:39 +0200

    Get adjacent tiles

--- js/board.js
+++ js/board.js
@@ -3,11 +3,10 @@ function Board( width, height ) {
 	this.width = width;
 	this.height = height;
 	this.tiles = Array( width * height );
-	for ( i=0; i < this.tiles.length; i++ ) {
+	for ( i = 0; i < this.tiles.length; i++ ) {
 		this.tiles[ i ] = null;
 	}
 }
-
 
 Board.prototype.placeTile = function( row, col, tile ) {
 	var pos = row * this.height + col;
@@ -15,13 +14,37 @@ Board.prototype.placeTile = function( row, col, tile ) {
 		throw Error( 'Cell not empty' );
 	}
 	this.tiles[ pos ] = tile;
-}
+};
 
 Board.prototype.getTurnsRemaining = function() {
 	return this.tiles.reduce( function (prev, tile) { 
 		return tile == null ? prev + 1 : prev; 
 	}, 0 );
 	return r;
-} 
+};
+
+/* return a "NSWE" object with the adjacent Tiles. Board edges are represented as null */
+Board.prototype.getAdjacentTiles = function( row, col ) {
+	var adjacent = {
+		N: null,
+		S: null,
+		W: null,
+		E: null
+	},
+	pos = row * this.height + col;
+	if ( row > 0 ) {
+		adjacent.N = this.tiles[ pos - this.width ];
+	}
+	if ( row < this.height - 1 ) {
+		adjacent.S = this.tiles[ pos + this.width ];
+	}
+	if ( col > 0 ) {
+		adjacent.W = this.tiles[ pos - 1 ];	
+	}
+	if ( col < this.width - 1 ) {
+		adjacent.E = this.tiles[ pos + 1 ];	
+	}
+	return adjacent;
+}
 
 module.exports = Board;
--- test/board_test.js
+++ test/board_test.js
@@ -1,6 +1,8 @@
 var expect = require( 'chai' ).expect,
 	Board = require( '../js/board' ),
-	Tile = require( '../js/tile' );
+	Tile = require( '../js/tile' ),
+	Colors = require( '../js/colors' ),
+	Shapes = require( '../js/shapes' );
 
 describe( 'Board', function () {
 
@@ -27,6 +29,61 @@ describe( 'Board', function () {
 			expect( function() {
 				b.placeTile( 0, 0, new Tile() );
 			} ).to.throw( 'Cell not empty' );
+		} );
+
+	} );
+
+	describe( '#getAdjacentTiles', function () {
+
+		var northTile = new Tile( Colors.RED, Shapes.SQUARE ),
+			southTile = new Tile( Colors.RED, Shapes.CIRCLE ),
+			westTile = new Tile( Colors.RED, Shapes.TRIANGLE ),
+			eastTile = new Tile( Colors.BLUE, Shapes.SQUARE);
+
+		it( 'returns null for empty board', function () {
+			var b = new Board( 6, 6 );
+			expect( b.getAdjacentTiles() ).to.deep.equal({
+				N: null,
+				S: null,
+				E: null,
+				W: null
+			});
+		} );
+
+		it( 'returns adjacent elements', function () {
+			var b = new Board( 6, 6 );
+			b.placeTile( 0, 1, northTile );
+			b.placeTile( 2, 1, southTile );
+			b.placeTile( 1, 0, westTile );
+			b.placeTile( 1, 2, eastTile );
+			expect( b.getAdjacentTiles( 1, 1 ) ).to.deep.equal({
+				N: northTile,
+				S: southTile,
+				W: westTile,
+				E: eastTile
+			});
+		} );
+
+		it( 'returns null on board corners', function () {
+			var b = new Board( 6, 6 );
+			// bottom right corner
+			b.placeTile( 4, 5, northTile );
+			b.placeTile( 5, 4, westTile );
+			// top left corner
+			b.placeTile( 1, 0, southTile );
+			b.placeTile( 0, 1, eastTile );
+			expect( b.getAdjacentTiles( 0, 0 ) ).to.deep.equal({
+				N: null,
+				S: southTile,
+				W: null,
+				E: eastTile
+			});
+			expect( b.getAdjacentTiles( 5, 5 ) ).to.deep.equal({
+				N: northTile,
+				S: null,
+				W: westTile,
+				E: null
+			});
 		} );
 
 	} );
